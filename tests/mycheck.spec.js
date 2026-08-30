f7e72811643756be26256452624982bef92692e3
Neeraj Kumar <nrjjrohila@gmail.com>
2026-08-30 11:19:12 +0530

    Add Playwright tests

---
+++ tests/mycheck.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('Test New Demo', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
+
+test('This is my second Test @smoke', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+})
